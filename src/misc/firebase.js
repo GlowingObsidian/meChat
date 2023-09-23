35fe437434cd25ec9b8dd2031edb6a3e30420a96
Shreyan Dey <deyshreyan24@outlook.com>
2023-09-23 13:14:48 +0530

    initialise firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDTd9WPkF6WueAtYDjx4Hv8Ho4OJU1Uirw',
+  authDomain: 'mechat-6e913.firebaseapp.com',
+  databaseURL:
+    'https://mechat-6e913-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'mechat-6e913',
+  storageBucket: 'mechat-6e913.appspot.com',
+  messagingSenderId: '928879238352',
+  appId: '1:928879238352:web:69afa9d20f17aab364d2d5',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
