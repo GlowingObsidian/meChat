1126c28e32437a2ce262d4a1be729cb74ca29d8f
Shreyan Dey <deyshreyan24@outlook.com>
2023-09-29 20:07:02 +0530

    start building sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import { DashboardToggle } from '../dashboard/DashboardToggle';
+
+export function Sidebar() {
+  return (
+    <div className="h-100 pt-2">
+      <div>
+        <DashboardToggle />
+      </div>
+      bottom
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,14 @@
+import { Col, Grid, Row } from 'rsuite';
+import { Sidebar } from '../components/Sidebar';
+
 export function Home() {
-  return <>Home</>;
+  return (
+    <Grid fluid className="h-100">
+      <Row>
+        <Col xs={24} md={8}>
+          <Sidebar />
+        </Col>
+      </Row>
+    </Grid>
+  );
 }
