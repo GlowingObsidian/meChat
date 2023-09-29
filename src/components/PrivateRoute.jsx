89f4240974f0720b441c0186d34f139780499f69
Shreyan Dey <deyshreyan24@outlook.com>
2023-09-29 14:05:45 +0530

    manage user with profile context

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,21 @@
 import { Route, Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import { useProfile } from '../context/profile.context';
+import { Container, Loader } from 'rsuite';
 
 export function PrivateRoute({ children, ...routeProps }) {
-  const profile = false;
-  if (!profile) {
+  const { profile, isLoading } = useProfile();
+
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (!profile && !isLoading) {
     return <Redirect to="/signin" />;
   }
+
   return <Route {...routeProps}>{children}</Route>;
 }
