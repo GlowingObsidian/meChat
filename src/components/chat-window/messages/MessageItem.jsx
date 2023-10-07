ed5f6d83db41e66fa1484ad13618bd9b8210d8ca
Shreyan Dey <deyshreyan24@outlook.com>
2023-10-08 00:49:16 +0530

    display user data with modal window

--- src/components/chat-window/messages/MessageItem.jsx
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -1,19 +1,24 @@
 import ProfileAvatar from '../../ProfileAvatar';
 import TimeAgo from 'timeago-react';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 export default function MessageItem({ message }) {
   const { author, createdAt, text } = message;
 
   return (
     <li className="padded mb-1">
-      <div className="d-flex align-item-center font-bolder mb-1">
+      <div className="d-flex align-items-center font-bolder mb-1">
         <ProfileAvatar
           src={author.avatar}
           name={author.name}
           className="ml-1"
           size="xs"
         />
-        <span className="ml-2">{author.name}</span>
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        />
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
---
+++ src/components/chat-window/messages/ProfileInfoBtnModal.jsx
@@ -0,0 +1,39 @@
+import { Button, Modal } from 'rsuite';
+import { useModalState } from '../../../misc/custom-hook';
+import ProfileAvatar from '../../ProfileAvatar';
+
+export default function ProfileInfoBtnModal({ profile, ...btnProps }) {
+  const shortName = profile.name.split(' ')[0];
+  const { name, avatar, createdAt } = profile;
+
+  const { isOpen, close, open } = useModalState();
+
+  const memberSince = new Date(createdAt).toLocaleDateString();
+
+  return (
+    <div>
+      <Button onClick={open} {...btnProps}>
+        {shortName}
+      </Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>{shortName} profile</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <ProfileAvatar
+            src={avatar}
+            name={name}
+            className="width-200 height-200 img-fullsize font-huge"
+          />
+          <h4 className="mt-2">{name}</h4>
+          <p>Member since {memberSince}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block onClick={close}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
